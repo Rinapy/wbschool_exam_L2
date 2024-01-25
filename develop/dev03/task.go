c7e51a021fadab8a55e050631509a002e1ca582a
Roman Minaev <romario123321@yandex.ru>
2024-01-25 22:28:40 +0300

    update dev03

--- develop/dev03/task.go
+++ develop/dev03/task.go
@@ -1,5 +1,17 @@
 package main
 
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"sort"
+	"strconv"
+	"strings"
+)
+
 /*
 === Утилита sort ===
 
@@ -24,3 +36,70 @@ package main
 
 Программа должна проходить все тесты. Код должен проходить проверки go vet и golint.
 */
+
+var (
+	k int
+	n bool
+)
+
+type Line struct {
+	Fields []string
+}
+
+type LineSlice []Line
+
+func (ls LineSlice) Len() int {
+	return len(ls)
+}
+
+func (ls LineSlice) Less(i, j int) bool {
+	if n {
+		val1, err := strconv.Atoi(ls[i].Fields[k])
+		val2, err := strconv.Atoi(ls[j].Fields[k])
+		if err != nil {
+			return ls[i].Fields[k] < ls[j].Fields[k]
+		}
+		return val1 < val2
+	}
+	return ls[i].Fields[k] < ls[j].Fields[k]
+}
+
+func (ls LineSlice) Swap(i, j int) {
+	ls[i], ls[j] = ls[j], ls[i]
+}
+
+func main() {
+	flag.IntVar(&k, "k", 0, "Индекс колнки по которой будет сортировка")
+	flag.BoolVar(&n, "n", false, "Индекс колнки по которой будет сортировка")
+	flag.Parse()
+	fmt.Println(k)
+	fmt.Println(n)
+	fileName := "test.txt"
+	lines := fillLineSlice(fileName)
+	sort.Sort(lines)
+	for _, line := range lines {
+		fmt.Println(line.Fields)
+	}
+}
+func fillLineSlice(filename string) LineSlice {
+	file, err := os.Open(filename)
+	defer file.Close()
+	if err != nil {
+		log.Println(err)
+		os.Exit(1)
+	}
+	reader := bufio.NewReader(file)
+	lineSlice := make(LineSlice, 0)
+	for {
+		lineText, err := reader.ReadString('\n')
+		if err == io.EOF {
+			break
+		} else if err != nil {
+			log.Println(err)
+		}
+
+		line := Line{strings.Fields(lineText)}
+		lineSlice = append(lineSlice, line)
+	}
+	return lineSlice
+}
