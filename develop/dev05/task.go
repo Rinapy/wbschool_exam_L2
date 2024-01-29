58ddeeb8ae88c0845920327d4eaaaed58e59ce11
Roman Minaev <romario123321@yandex.ru>
2024-01-29 16:07:43 +0300

    create task dev05

--- develop/dev05/task.go
+++ develop/dev05/task.go
@@ -4,7 +4,9 @@ import (
 	"bufio"
 	"flag"
 	"fmt"
+	"io"
 	"os"
+	"path/filepath"
 	"strings"
 )
 
@@ -26,145 +28,246 @@ import (
 Программа должна проходить все тесты. Код должен проходить проверки go vet и golint.
 */
 
-func main() {
-	// Определение флагов командной строки
-	after := flag.Int("A", 0, "печатать +N строк после совпадения")
-	before := flag.Int("B", 0, "печатать +N строк до совпадения")
-	context := flag.Int("C", 0, "печатать ±N строк вокруг совпадения")
-	count := flag.Bool("c", false, "количество строк")
-	ignoreCase := flag.Bool("i", false, "игнорировать регистр")
-	invert := flag.Bool("v", false, "вместо совпадения, исключать")
-	fixed := flag.Bool("F", false, "точное совпадение со строкой, не паттерн")
-	lineNum := flag.Bool("n", false, "напечатать номер строки")
-	// Парсинг флагов командной строки
+var (
+	A int
+	B int
+	C int
+	c bool
+	i bool
+	v bool
+	F bool
+	n bool
+)
+
+type Line struct {
+	Text string
+}
+type lineSlice []Line
+
+type File struct {
+	name     string
+	lines    lineSlice
+	strCount int
+	findStr  []int
+}
+
+type FileSlice []File
+
+func parseNameOrPattern(arg string) []string {
+	fs := make([]string, 0)
+	isPattern := false
+	for _, val := range arg {
+		if string(val) == "*" {
+			isPattern = true
+			break
+		}
+	}
+	if isPattern {
+		files, err := filepath.Glob(arg)
+		if err != nil {
+			fmt.Println("Ошибка поиска файлов:", err)
+		}
+
+		for _, file := range files {
+			fs = append(fs, file)
+		}
+	} else {
+		fs = append(fs, arg)
+	}
+	return fs
+}
+
+func parseFlags() ([]string, string) {
+	flag.IntVar(&A, "A", 0, "\"after\" печатать +N строк после совпадения")
+	flag.IntVar(&B, "B", 0, "\"before\" печатать +N строк до совпадения")
+	flag.IntVar(&C, "C", 0, "\"context\" (A+B) печатать ±N строк вокруг совпадения")
+	flag.BoolVar(&c, "c", false, "\"count\" (количество строк)")
+	flag.BoolVar(&i, "i", false, "\"ignore-case\" (игнорировать регистр)")
+	flag.BoolVar(&v, "v", false, "\"invert\" (вместо совпадения, исключать)")
+	flag.BoolVar(&F, "F", false, "\"fixed\", точное совпадение со строкой, не паттерн")
+	flag.BoolVar(&n, "n", false, "\"line num\", печатать номер строки")
 	flag.Parse()
+	fs := parseNameOrPattern(flag.Arg(1))
+	str := flag.Arg(0)
+	return fs, str
+}
 
-	// Получение паттерна и файлов из аргументов командной строки
-	pattern := flag.Arg(0)
-	files := flag.Args()[1:]
+func fillFileSlice(filenames []string) (FileSlice, error) {
+	fileSlice := FileSlice{}
 
-	// Проверка наличия паттерна и файлов
-	if pattern == "" || len(files) == 0 {
-		fmt.Println("Использование: grep [опции] паттерн файлы...")
-		flag.PrintDefaults()
-		os.Exit(1)
+	for _, filename := range filenames {
+		file, err := os.Open(filename)
+		if err != nil {
+			return nil, err
+		}
+		defer file.Close()
+
+		lines, err := readLines(file)
+		if err != nil {
+			return nil, err
+		}
+
+		fileInfo := File{
+			name:     filename,
+			lines:    lines,
+			strCount: len(lines),
+			findStr:  []int{},
+		}
+
+		fileSlice = append(fileSlice, fileInfo)
 	}
 
-	// Открытие файлов и фильтрация строк
-	for _, file := range files {
-		f, err := os.Open(file)
+	return fileSlice, nil
+}
+
+func readLines(file *os.File) ([]Line, error) {
+	reader := bufio.NewReader(file)
+	var lines []Line
+
+	for {
+		lineText, err := reader.ReadString('\n')
 		if err != nil {
-			fmt.Printf("Ошибка при открытии файла %s: %v\n", file, err)
+			if err == io.EOF {
+				break
+			} else {
+				return nil, err
+			}
+		}
+
+		cleanStr := strings.TrimRight(lineText, "\r\n")
+		line := Line{Text: cleanStr}
+		lines = append(lines, line)
+	}
+
+	return lines, nil
+}
+
+func printer(fileSlice FileSlice) {
+	match := "Match -- %v\n"
+	before := "Before -- %v\n"
+	after := "After  -- %v\n"
+	matchN := "Match -- %v [%v]\n"
+	beforeN := "Before -- %v [%v]\n"
+	afterN := "After  -- %v [%v]\n"
+
+	for _, file := range fileSlice {
+		if len(file.findStr) > 1 {
+			fmt.Printf("---------------------------%v---------------------------\n", file.name)
+		}
+
+		if c {
+			fmt.Printf(match, len(file.findStr))
 			continue
 		}
-		defer f.Close()
 
-		scanner := bufio.NewScanner(f)
-		lineNum := 0
-		printLine := func(line string) {
-			if *lineNum {
-				fmt.Printf("%d:", lineNum)
-			}
-			fmt.Println(line)
-		}
-
-		// Флаги "before" и "context"
-		beforeCounter := 0
-		contextCounter := 0
-		beforeLines := []string{}
-		contextLines := []string{}
-
-		for scanner.Scan() {
-			line := scanner.Text()
-			lineNum++
-
-			match := false
-			if *ignoreCase {
-				match = strings.Contains(strings.ToLower(line), strings.ToLower(pattern))
-			} else if *fixed {
-				match = line == pattern
-			} else {
-				match = strings.Contains(line, pattern)
+		for _, idx := range file.findStr {
+			if C > 0 {
+				A = C
+				B = C
 			}
 
-			if *invert {
-				match = !match
-			}
-
-			// Флаг "before"
-			if *before > 0 && beforeCounter > 0 {
-				beforeLines = append(beforeLines, line)
-				if len(beforeLines) > *before {
-					beforeLines = beforeLines[1:]
-				}
-			}
-
-			// Флаг "context"
-			if *context > 0 && contextCounter > 0 {
-				contextLines = append(contextLines, line)
-				if len(contextLines) > *context {
-					contextLines = contextLines[1:]
-				}
-			}
-
-			// Печать совпадающих строк
-			if match {
-				if *before > 0 {
-					for _, beforeLine := range beforeLines {
-						printLine(beforeLine)
-					}
-				}
-
-				if *context > 0 {
-					for _, contextLine := range contextLines {
-						printLine(contextLine)
-					}
-				}
-
-				printLine(line)
-
-				if *after > 0 {
-					afterCounter := 0
-					for scanner.Scan() {
-						afterLine := scanner.Text()
-						lineNum++
-						if *lineNum {
-							fmt.Printf("%d:", lineNum)
+			if B > 0 {
+				for iB := 1; iB <= B; iB++ {
+					beforeIdx := idx - iB
+					if beforeIdx >= 0 {
+						if n {
+							fmt.Printf(beforeN, file.lines[beforeIdx].Text, beforeIdx+1)
+							continue
 						}
-						fmt.Println(afterLine)
-						afterCounter++
-						if afterCounter >= *after {
-							break
-						}
+						fmt.Printf(before, file.lines[beforeIdx].Text)
 					}
 				}
-
-				// Флаг "count"
-				if *count {
-					break
-				}
 			}
 
-			// Сброс счётчиков для опций "before" и "context"
-			if match {
-				beforeCounter = 0
-				contextCounter = 0
-				beforeLines = []string{}
-				contextLines = []string{}
+			if n {
+				fmt.Printf(matchN, file.lines[idx].Text, idx+1)
 			} else {
-				beforeCounter++
-				contextCounter++
-				if *before > 0 && beforeCounter > *before {
-					beforeCounter = *before
-				}
-				if *context > 0 && contextCounter > *context {
-					contextCounter = *context
+				fmt.Printf(match, file.lines[idx].Text)
+			}
+
+			if A > 0 {
+				for iA := 1; iA <= A; iA++ {
+					afterIdx := idx + iA
+					if n {
+						fmt.Printf(afterN, file.lines[afterIdx].Text, afterIdx+1)
+						continue
+					}
+					fmt.Printf(after, file.lines[afterIdx].Text)
 				}
 			}
-		}
-
-		if err := scanner.Err(); err != nil {
-			fmt.Printf("Ошибка при чтении файла %s: %v\n", file, err)
 		}
 	}
+}
+
+func Finder(fileSlice FileSlice, text string) {
+	found := false
+	for idx := range fileSlice {
+		file := &fileSlice[idx]
+		for idx, line := range file.lines {
+			if !F {
+				if !v {
+					if (i && strings.Contains(strings.ToLower(line.Text), strings.ToLower(text))) ||
+						(!i && strings.Contains(line.Text, text)) {
+						file.findStr = append(file.findStr, idx)
+						found = true
+					}
+				} else {
+					if A != 0 || B != 0 || C != 0 {
+						A = 0
+						B = 0
+						C = 0
+						fmt.Println("Использование флагов -A -B -C при флаге -v недопустимо, данные флаги отключены.")
+					}
+
+					if (i && !strings.Contains(strings.ToLower(line.Text), strings.ToLower(text))) ||
+						(!i && !strings.Contains(line.Text, text)) {
+						file.findStr = append(file.findStr, idx)
+						found = true
+					}
+				}
+			} else {
+				if !v {
+					if (i && strings.ToLower(line.Text) == strings.ToLower(text)) ||
+						(!i && line.Text == text) {
+						file.findStr = append(file.findStr, idx)
+						found = true
+
+					}
+				} else {
+					if A != 0 || B != 0 || C != 0 {
+						A = 0
+						B = 0
+						C = 0
+						fmt.Println("Использование флагов -A -B -C при флаге -v недопустимо, данные флаги отключены.")
+					}
+
+					if (i && strings.ToLower(line.Text) != strings.ToLower(text)) ||
+						(!i && line.Text != text) {
+						file.findStr = append(file.findStr, idx)
+						found = true
+					}
+				}
+			}
+
+		}
+
+	}
+	if found {
+		printer(fileSlice)
+	} else {
+		fmt.Println("Совпадений не найдено")
+	}
+
+}
+
+func main() {
+	fsn, str := parseFlags()
+	//fmt.Println(fsn)
+	//fmt.Println(str)
+	fs, err := fillFileSlice(fsn)
+	if err != nil {
+		fmt.Println(err)
+	}
+	Finder(fs, str)
+
 }
