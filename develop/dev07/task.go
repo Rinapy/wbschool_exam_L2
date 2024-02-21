fd546142893ce1b46ae52e85e26e61fa10842b9f
Roman Minaev <romario123321@yandex.ru>
2024-02-21 14:34:32 +0300

    update dev07

--- develop/dev07/task.go
+++ develop/dev07/task.go
@@ -41,11 +41,46 @@ start := time.Now()
 
 fmt.Printf(“fone after %v”, time.Since(start))
 */
-func main() {
-	//Определение функции:
-	var or func(channels ...<-chan interface{}) <-chan interface{}
+// Функция or принимает несколько каналов для объединения и возвращает объединенный канал
+func or(channels ...<-chan interface{}) <-chan interface{} {
+	switch len(channels) {
+	case 0:
+		// Если не передано ни одного канала, возвращается nil
+		return nil
+	case 1:
+		// Если передан только один канал, он возвращается без изменений
+		return channels[0]
+	}
 
-	//Пример использования функции:
+	orDone := make(chan interface{})
+	go func() {
+		defer close(orDone)
+
+		switch len(channels) {
+		case 2:
+			// Если передано два канала, используется select для объединения
+			select {
+			case <-channels[0]:
+			case <-channels[1]:
+			}
+		default:
+			// Если передано более двух каналов, рекурсивно вызывается функция or
+			selectCases := make([]<-chan interface{}, len(channels)-1)
+			for i := range selectCases {
+				selectCases[i] = channels[i+1]
+			}
+			select {
+			case <-channels[0]:
+			case <-or(selectCases...):
+			}
+		}
+	}()
+
+	return orDone
+}
+
+func main() {
+	// Функция sig создает канал, который закроется после заданного времени
 	sig := func(after time.Duration) <-chan interface{} {
 		c := make(chan interface{})
 		go func() {
@@ -55,14 +90,18 @@ func main() {
 		return c
 	}
 
+	// Замеряется время начала выполнения
 	start := time.Now()
+
+	// Используется or для ожидания закрытия одного из каналов
 	<-or(
 		sig(2*time.Hour),
 		sig(5*time.Minute),
-		sig(1*time.Second),
+		sig(60*time.Second),
 		sig(1*time.Hour),
 		sig(1*time.Minute),
 	)
 
-	fmt.Printf("fone after %v", time.Since(start))
+	// Выводится время, прошедшее с начала выполнения
+	fmt.Printf("Done after %v", time.Since(start))
 }
